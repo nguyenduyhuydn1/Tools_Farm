d1c5834eab4b023d76fc8418e7a12abf6a06fab3
Your Name <your.email@example.com>
2024-10-16 18:33:26 +0700

    o

--- tools/1-kucoin.js
+++ tools/1-kucoin.js
@@ -13,7 +13,7 @@ let xPosition = 0;
 let yPosition = 0;
 const MainBrowser = async (countFolder) => {
     try {
-        const browser = await runPuppeteer(`C:\\Users\\Huy\\AppData\\Local\\Google\\Chrome\\User Data\\Profile ${countFolder + 100}`, ['--window-size=600,300', `--window-position=${xPosition},${yPosition}`], proxyUrl);
+        const browser = await runPuppeteer(`C:\\Users\\Huy\\AppData\\Local\\Google\\Chrome\\User Data\\memefi ${countFolder + 300}`, ['--window-size=400,300', `--window-position=${xPosition},${yPosition}`], proxyUrl);
         xPosition += 300;
         if (xPosition + 300 > 1920) {
             xPosition = 0;
@@ -37,6 +37,31 @@ const MainBrowser = async (countFolder) => {
         await page.goto("https://web.telegram.org/k/#@xkucoinbot");
         const [src, iframe] = await checkIframeAndClick(page);
         await page.goto(src);
+        await sleep(1000);
+
+        await page.evaluate(() => {
+            window.check = true;
+            window.silent = true;
+
+            let silent = document.querySelector(".iconSound--LlCOj");
+            if (silent && window.silent) {
+                setTimeout(() => {
+                    silent.click();
+                    window.silent = false;
+                }, 3000);
+            }
+
+            setInterval(() => {
+                let processElement = document.querySelector("#root .process--W73kB")
+                const count = parseInt(processElement.textContent, 10);
+                if (window.check) {
+                    document.querySelector(".frog--GPU1j").click();
+                    if (count < 10) window.check = false;
+                } else {
+                    if (count > 2800) window.check = true;
+                }
+            }, 400);
+        },);
     } catch (error) {
         console.error("Error:", error.message);
     }
